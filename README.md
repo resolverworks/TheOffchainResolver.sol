72b12996c8f8dc017b1651b28dc800de8cc48a4f
Andrew Raffensperger <raffy@me.com>
2024-02-15 13:42:38 -0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # TheOffchainResolver.sol
-Universal Offchain (ENS+DNS) Resolver
+The Universal Offchain (ENS+DNS) Resolver
+
+*Under construction: currently only the DNS functionality is deployed.*
+
+* [Contract](./contracts/TheOffchainResolverMini.sol)
+* Deployments:
+	* [`mainnet:0xa4407E257Aa158C737292ac95317a29b4C90729D`](https://etherscan.io/address/0xa4407E257Aa158C737292ac95317a29b4C90729D#code)
+	* [`sepolia:0x179Be112b24Ad4cFC392eF8924DfA08C20Ad8583`](https://sepolia.etherscan.io/address/0xedb18cd8d9d6af54c4ac1fbdbf2e098f413c3fe9#code)
+* Protocol
+	* [Signing Logic](https://github.com/adraffy/ezccip.js/blob/4f05546110185e8016708ad65db8b96e259f8148/src/index.js#L40)
+	* Implementation: [resolverworks/ezccip.js](https://github.com/adraffy/ezccip.js)
+* DNS Example: `ezccip.raffy.xyz` → [DNS TXT](https://mxtoolbox.com/SuperTool.aspx?action=txt%3aezccip.raffy.xyz&run=toolpage) → [ENS](https://app.ens.domains/ezccip.raffy.xyz) / [Resolver](https://adraffy.github.io/ens-normalize.js/test/resolver.html#ezccip.raffy.xyz)
+
+### Context Format
+
+`CONTEXT` = `${SIGNER} ${SERVER_ENDPOINT}`
+
+* `SIGNER` = `0x`-prefixed public address of signing key
+* `SERVER_ENDPOINT` = URL of your CCIP-Read server
+
+Example: `0xd00d726b2aD6C81E894DC6B87BE6Ce9c5572D2cd https://raffy.xyz/ezccip/`
+
+### Setup
+
+* *"I have a DNS name"* → `TXT` = `ENS1 0xa4407E257Aa158C737292ac95317a29b4C90729D ${CONTEXT}`
+* *"I have an ENS name"* → [PublicResolver](https://etherscan.io/address/resolver.ens.eth)`.setText("ccip.context", CONTEXT)`
+
+That's it! 🎉️ 
---
+++ contracts/TheOffchainResolverMini.sol
@@ -0,0 +1,71 @@
+/// @author raffy.eth
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {ECDSA} from "@openzeppelin/contracts@4.8.2/utils/cryptography/ECDSA.sol";
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import {ENS} from "@ensdomains/ens-contracts/contracts/registry/ENS.sol";
+import {IExtendedResolver} from "@ensdomains/ens-contracts/contracts/resolvers/profiles/IExtendedResolver.sol";
+import {IExtendedDNSResolver} from "@ensdomains/ens-contracts/contracts/resolvers/profiles/IExtendedDNSResolver.sol";
+import {ITextResolver} from "@ensdomains/ens-contracts/contracts/resolvers/profiles/ITextResolver.sol";
+import {BytesUtils} from "@ensdomains/ens-contracts/contracts/wrapper/BytesUtils.sol";
+
+error OffchainLookup(address sender, string[] urls, bytes callData, bytes4 callbackFunction, bytes extraData);
+
+contract TheOffchainResolverMini is IERC165, IExtendedDNSResolver {
+	using BytesUtils for bytes;
+
+	function supportsInterface(bytes4 x) external pure returns (bool) {
+		return x == type(IERC165).interfaceId
+			|| x == type(IExtendedDNSResolver).interfaceId
+			|| x == 0xb3fb17f9; 
+	}
+
+	function resolve(bytes calldata name, bytes calldata data, bytes calldata context) external view returns (bytes memory) {
+		require(bytes(context).length >= 51, "content");
+		address signer = address(bytes20(parseHex(bytes(context)[2:42])));
+		string memory endpoint = string(context[43:]);
+		string[] memory urls = new string[](1);
+		urls[0] = endpoint;
+		bytes memory call = abi.encodeWithSelector(IExtendedResolver.resolve.selector, name, data);
+		revert OffchainLookup(address(this), urls, call, this.buggedCallback.selector, abi.encode(abi.encode(call, signer), address(this)));
+	}
+	function resolveCallback(bytes memory response, bytes memory extraData) public view returns (bytes memory) {
+		(bytes memory sig, uint64 expires, bytes memory result) = abi.decode(response, (bytes, uint64, bytes));
+		require(expires > block.timestamp, "expired");
+		(bytes memory call, address signer) = abi.decode(extraData, (bytes, address));
+		bytes32 hash = keccak256(abi.encodePacked(address(this), expires, keccak256(call), keccak256(result)));
+		address signed = ECDSA.recover(hash, sig);
+		require(signed == signer, "untrusted");
+		return result;
+	}
+	function buggedCallback(bytes calldata response, bytes calldata buggedExtraData) external view returns (bytes memory) {
+		return resolveCallback(response, abi.decode(buggedExtraData, (bytes)));
+	}
+
+	function parseHex(bytes memory h) internal pure returns (bytes memory v) {
+		unchecked {
+			require((h.length & 1) == 0, "ragged");
+			v = new bytes(h.length >> 1);
+			for (uint256 i; i < h.length; i += 2) {
+				uint256 u = radixFromChar(uint8(h[i]));
+				uint256 l = radixFromChar(uint8(h[i + 1]));
+				v[i >> 1] = bytes1(uint8((u << 4) | l));
+			}
+		}
+	}
+	function radixFromChar(uint256 ch) internal pure returns (uint256) {
+		unchecked {
+			if (ch >= 97 && ch <= 102) {
+				return ch - 87; // [a-f] => 10-15
+			} else if (ch >= 48 && ch <= 57) {
+				return ch - 48; // [0-9] => 0-9
+			} else if (ch >= 65 && ch <= 70) {
+				return ch - 55; // [A-F] => 10-15
+			} else {
+				revert("bad hex");
+			}
+		}
+	}
+
+}
