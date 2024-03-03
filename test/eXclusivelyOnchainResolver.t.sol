537b02cf469128c1e5eb4ad7c48d8628a321ca6f
Andrew Raffensperger <raffy@me.com>
2024-03-03 12:32:16 -0800

    convert to foundry

---
+++ test/eXclusivelyOnchainResolver.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Counter} from "../src/Counter.sol";
+
+contract XORTest is Test {
+
+	function setUp() public {
+		console2.log("nice chonk");
+	}
+
+}
